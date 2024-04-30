aea7baee9894380bc2344ad14fe0ee0920d8264c
outrosdiasvirao <outrosdiasvirao@yahoo.com.br>
2024-04-30 20:49:45 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import * as M from 'materialize-css';
+
+import { AfterViewInit,
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit }
+ from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, 
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+
+  /*Para o Materialize*/
+  /*ViewChild: pai acessa o component filho dentro da classe */
+  @ViewChild('mobile') sideNav?: ElementRef;
+
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement);
+  }
   title = 'projeto';
 }
---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primeiro-componente',
+  standalone: true,
+  imports: [],
+  templateUrl: './primeiro-componente.component.html',
+  styleUrl: './primeiro-componente.component.css'
+})
+export class PrimeiroComponenteComponent {
+
+}
